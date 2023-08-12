7e12fabf8d8ddc29cec04afb783500e73dfb1d84
FLJK1011 <136923911+FLJK1011@users.noreply.github.com>
2023-08-12 17:20:52 +0330

    Create squareArea.cpp

---
+++ C++/squareArea.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main(){
+  cout << "Enter Side of Square : ";
+  int squaceArea;
+  cin >> squareArea;
+  squareArea *= squareArea
+  cout << "Your Square Area is : " << squareArea << endl;
+}
