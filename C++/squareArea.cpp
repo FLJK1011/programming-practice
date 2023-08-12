37e39b0e5f64337c9b969154e65327cc983175a9
FLJK1011 <136923911+FLJK1011@users.noreply.github.com>
2023-08-12 17:22:20 +0330

    fixing the bugs

--- C++/squareArea.cpp
+++ C++/squareArea.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 main(){
-  cout << "Enter Side of Square : ";
-  int squaceArea;
-  cin >> squareArea;
-  squareArea *= squareArea
-  cout << "Your Square Area is : " << squareArea << endl;
+    cout << "Enter Side of Square : ";
+    int squareArea;
+    cin >> squareArea;
+    squareArea *= squareArea;
+    cout << "Your Square Area is : " << squareArea << endl;
 }
